c8c7c51c0fa8c367ad6a288f46ee887bdf255179
PedroMarMol <martosmoleropedro@gmail.com>
2023-01-04 19:09:10 +0100

    toggle panel collapse

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,11 +2,15 @@ import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 import { useState } from "react";
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
 
     const handleClick = (nextIndex) => {
-        setExpandedIndex(nextIndex);
-    }
+        if (expandedIndex === nextIndex) {
+            setExpandedIndex(-1);
+        }   else {
+            setExpandedIndex(nextIndex);
+        }
+    };
 
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
