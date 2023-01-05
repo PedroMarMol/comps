5c7bc92fb75b4f9b94ac6d68f76f36e6e0131a81
PedroMarMol <martosmoleropedro@gmail.com>
2023-01-05 12:48:29 +0100

    implement controlled comp

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,19 +1,27 @@
 import { useState } from "react";
 
-function Dropdown({ options }){
+function Dropdown({ options, selection, onSelect }){
     const [isOpen, setIsOpen] = useState(false);
+    console.log(selection)
 
     const handleClick = () => {
         setIsOpen((currentIsOpen) => !currentIsOpen);
     };
 
+    const handleOptionClick = (option) => {
+        // close the dropdown
+        setIsOpen(false);
+        // figure out which option was selected
+        onSelect(option);
+    };
+
     const renderedOptions = options.map((option) => {
-        return <div key={option.value}>{option.label}</div>
+        return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
     });
 
     return (
         <div>
-            <div onClick={handleClick}>Select...</div>
+            <div onClick={handleClick}>{selection.label || "Select..."}</div>
             {isOpen && <div>{renderedOptions}</div>}
         </div>
     );
