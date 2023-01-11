12d0133fc1387709b878e3a6195de99ed0e718d1
PedroMarMol <martosmoleropedro@gmail.com>
2023-01-11 13:42:20 +0100

    modal styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,14 @@ function Modal({ onClose, children, actionBar }) {
     return ReactDOM.createPortal(
         <div>
             <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-            <div className="absolute inset-40 p-10 bg-white">{children}{actionBar}</div>
+            <div className="absolute inset-40 p-10 bg-white">
+                <div className="flex flex-col justify-between h-full">
+                    {children}
+                    <div className="flex justify-end">
+                        {actionBar}
+                    </div>
+                </div>
+            </div>
         </div>,
         document.querySelector('.modal-container')
     );
