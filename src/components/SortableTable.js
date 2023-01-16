178107da8a2441880b8f55a4d578191aaf2f4a5f
Pedro Martos <martosmoleropedro@gmail.com>
2023-01-16 09:57:34 +0100

    determine icon set

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -26,7 +26,7 @@ function SortableTable(props) {
 
         return {
             ...column,
-            header: () => <th onClick={() => handleClick(column.label)}>{column.label} IS SORTABLE</th>
+            header: () => <th onClick={() => handleClick(column.label)}>{getIcons(column.label, sortBy, sortOrder)}{column.label}/th>
         };
     });
 
@@ -56,6 +56,20 @@ function SortableTable(props) {
             <Table {...props} data={sortedData} config={updatedConfig} />;
         </div>
     );
+}
+
+function getIcons(label, sortBy, sortOrder) {
+    if (label !== sortBy) {
+        return 'show both icons'; 
+    }
+
+    if (sortOrder === null) {
+        return 'show both icons';
+    } else if (sortOrder === 'asc') {
+        return 'show up icon';
+    } else if (sortOrder === 'desc') {
+        return 'show down icon';
+    }
 }
 
 export default SortableTable;
