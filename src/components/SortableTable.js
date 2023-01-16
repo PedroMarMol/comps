e185ae94dbafe6e3067ad31045e405218f4b83b7
Pedro Martos <martosmoleropedro@gmail.com>
2023-01-16 10:37:17 +0100

    reset sort order

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -8,6 +8,12 @@ function SortableTable(props) {
     const { config, data } = props;
 
     const handleClick = (label) => {
+        if (sortBy && label !=== sortBy) {
+            setSortOrder('asc');
+            setSortBy(label);
+            return;
+        }
+
         if (sortOrder === null) {
             setSortOrder('asc');
             setSortBy(label);
@@ -52,13 +58,7 @@ function SortableTable(props) {
         });
     }
 
-    return (
-        <div>
-            {sortOrder} - {sortBy}
-            <Table {...props} data={sortedData} config={updatedConfig} />;
-        </div>
-    );
-}
+    return <Table {...props} data={sortedData} config={updatedConfig} />;
 
 function getIcons(label, sortBy, sortOrder) {
     if (label !== sortBy) {
