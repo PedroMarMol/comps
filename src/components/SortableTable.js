af36b89c92c9f60998222f9ef7e2ff849556a193
Pedro Martos <martosmoleropedro@gmail.com>
2023-01-16 11:45:50 +0100

    sorting hook

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,30 +1,15 @@
 import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
-import { useState } from "react";
 import Table from "./Table";
+import useSort from '../hooks/use-sort';
 
 function SortableTable(props) {
-    const [sortOrder, setSortOrder] = useState(null);
-    const [sortBy, setSortBy] = useState(null);
     const { config, data } = props;
-
-    const handleClick = (label) => {
-        if (sortBy && label !=== sortBy) {
-            setSortOrder('asc');
-            setSortBy(label);
-            return;
-        }
-
-        if (sortOrder === null) {
-            setSortOrder('asc');
-            setSortBy(label);
-        } else if (sortOrder === 'asc') {
-            setSortOrder('desc');
-            setSortBy(label);
-        } else if (sortOrder === 'desc') {
-            setSortOrder(null);
-            setSortBy(null);
-        }
-    };
+    const {
+        sortOrder,
+        sortBy,
+        sortedData,
+        setSortColumn,
+    } = useSort(data, config);
 
     const updatedConfig = config.map((column) => {
         if (!column.sortValue) {
@@ -33,32 +18,12 @@ function SortableTable(props) {
 
         return {
             ...column,
-            header: () => <th className='cursor-pointer hover:bg-gray-100' onClick={() => handleClick(column.label)}><div className='flex items-center'>{getIcons(column.label, sortBy, sortOrder)}{column.label}</div>/th>
+            header: () => <th className='cursor-pointer hover:bg-gray-100' onClick={() => setSortColumn(column.label)}><div className='flex items-center'>{getIcons(column.label, sortBy, sortOrder)}{column.label}</div>/th>
         };
     });
 
-    {/* only sort data if (sortOrder && sortBy) !== null
-    copy of 'data' prop
-    fetch correct sortValue function and use it for sorting */}
-
-    let sortedData = data;
-    if (sortOrder && sortBy) {
-        const { sortValue } = config.find(column => column.label === sortBy);
-        sortedData = [...data].sort((a,b) => {
-            const valueA = sortValue(a);
-            const valueB = sortValue(b);
-
-            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-
-            if (typeof valueA === 'string') {
-                return valueA.localeCompare(valueB) * reverseOrder;
-            } else {
-                return (valueA - valueB) * reverseOrder
-            }
-        });
-    }
-
-    return <Table {...props} data={sortedData} config={updatedConfig} />;q
+    return <Table {...props} data={sortedData} config={updatedConfig} />;
+}
 
 function getIcons(label, sortBy, sortOrder) {
     if (label !== sortBy) {
